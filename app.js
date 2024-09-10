177c78119e0d239f0765fbcf276cea26c70e070b
ManzoorJamali <jamalimanzoor05@gmail.com>
2024-09-10 16:35:55 +0500

    ok

---
+++ app.js
@@ -0,0 +1,53 @@
+// let btn = document.getElementById('add_btn');
+// btn.addEventListener('click', add);
+// function add(e){
+//    let currentbtn = e.currentTarget
+//    let currentInput = currentbtn.previousElementSibling
+
+//    if(currentInput.value == ""){
+//      alert('please insert')   ``
+//      end
+
+
+//    }
+
+ //  let newLi = document.createElement('li')
+
+  //  newLi.classList.add('list-group-item')
+  //  newLi.textContent = currentInput.value
+
+  //  let newul = document.getElementById('newul')
+  //  newul.appendChild(newLi)
+  //  currentInput.value = ""
+
+
+let inputs = document.getElementById('inp')
+let text = document.querySelector('.text')
+
+function add() {
+  if(inputs.value == ""){
+    alert("insert chapter");
+    
+ }
+  else{
+    let newel = document.createElement('ul')
+    newel.innerHTML = `${inputs.value}<li class="list-group-item" ></li><i class = "fa-solid fa-trash d-flex justify-content-end"></i>`;
+    text.appendChild(newel);
+    inputs.value = ""
+
+    newel.querySelector('i').addEventListener('click', remove);
+    function remove(){
+      newel.remove();
+    }
+
+
+  }
+}
+
+
+
+
+ 
+   
+ 
+
